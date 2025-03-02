5a06f8637bdd6c5dcaf951939abbdd26ce397f05
BIJIT DEB <bijitdeb0703@gmail.com>
2025-03-02 12:24:03 +0600

    card btn add , clear history work, and blog update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// theme change
+function theme() {
+    const colors = [
+        "bg-gray-100 ",
+        "bg-blue-200",
+        "bg-amber-50",
+        "bg-slate-50",
+        "bg-green-100",
+        "bg-yellow-100",
+        "bg-sky-100",
+        "bg-red-100",
+        "bg-teal-100",
+        "bg-lime-100",
+    ];
+    document.getElementById("theme-btn").addEventListener("click", function () {
+        document.body.className = colors[Math.floor(Math.random() * colors.length)];
+    })
+}
+
+theme();
+
 // cureent date function
 function cureentDate() {
     let date = new Date();
@@ -5,14 +26,13 @@ function cureentDate() {
     let optionsDate = { month: "short", day: "2-digit", year: "numeric" };
 
     let weekday = date.toLocaleDateString("en-US", optionsWeekday);
-    let day = date.toLocaleDateString("en-US", optionsDate);
+    let day = date.toLocaleDateString("en-US", optionsDate).replace(",", "");
 
     document.getElementById("today").innerHTML = `
-    ${weekday},
+    ${weekday}
     <br>
     <span class="font-bold">${day}</span>
     `
-    return;
 }
 cureentDate()
 
@@ -22,18 +42,53 @@ document.getElementById("blog-btn").addEventListener("click", function () {
 })
 
 
-// card btn 
-const taskButtons = document.querySelectorAll(".task-btn")
+// card btn
+const completedTasks = document.getElementById('completed-tasks');
+const countTask = document.getElementById('count-task');
 
-for (let i = 0; i < taskButtons.length; i++) {
-    const taskBtn = taskButtons[i];
-    taskBtn.addEventListener("click", function () {
+let date = new Date();
+const time = date.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+});
+
+const cards = document.querySelectorAll(".card-body")
+for (let card of cards) {
+    const btn = card.querySelector(".task-btn")
+
+    btn.addEventListener("click", function (event) {
         alert("Board Updated Successfully");
 
-        taskBtn.classList.remove("btn-active");
-        taskBtn.classList.add("btn-disabled");
+        btn.classList.remove("btn-active");
+        btn.classList.add("btn-disabled");
 
+        completedTasks.innerHTML = parseInt(completedTasks.innerText) + 1;
+
+        countTask.innerHTML = parseInt(countTask.innerText) - 1;
+
+        if (countTask.innerHTML === "0") {
+            alert("Congratulations! You have completed all the task")
+        }
+
+        const paragraph = document.createElement("p")
+        const title = card.querySelector(".card-title").innerText
         
+        paragraph.innerText = `You have Complete the task ${title} at ${time}`
+
+        paragraph.classList.add("bg-[#F4F7FF]", "p-4","m-4", "rounded-2xl");
+
+        const history = document.getElementById("history")
+        
+        history.append(paragraph);
     })
+
 }
 
+
+// clear history 
+document.getElementById("clear-btn").addEventListener("click", function () {
+    const history = document.getElementById("history")
+    history.innerHTML= ""
+})
